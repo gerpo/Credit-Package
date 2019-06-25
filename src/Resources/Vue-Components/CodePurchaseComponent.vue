8045c197156686e967a247a3bc22487cf2472d68
Adam Marciniak <adamarc@web.de>
2019-06-25 22:37:49 +0200

    WIP

--- src/Resources/Vue-Components/CodePurchaseComponent.vue
+++ src/Resources/Vue-Components/CodePurchaseComponent.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="code-purchase form-inline">
         <p class="lead w-100 mb-1">{{ $tv('DmsCredits::code.redeem_code') }}:</p>
-        <input type="text" title="redeem code" class="form-control flex-grow-1 mr-2" v-model="code.code">
-        <button class="btn btn-primary" @click="redeemCode">Redeem</button>
+        <input class="form-control flex-grow-1 mr-2" id="code-redeem-field" title="redeem code" type="text"
+               v-model="code.code">
+        <button @click="redeemCode" class="btn btn-primary">Redeem</button>
     </div>
 </template>
 
@@ -14,6 +15,9 @@
                 code: ''
             }
         }),
+        mounted() {
+            document.getElementById('code-redeem-field').focus();
+        },
         methods: {
             async redeemCode() {
                 if (!this.code.code) {
