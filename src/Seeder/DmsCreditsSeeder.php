0e990b9da2f102069ee3ef11b685d1cbfb5507d4
Adam Marciniak <adamarc@web.de>
2019-07-27 23:15:21 +0200

    WIP

--- src/Seeder/DmsCreditsSeeder.php
+++ src/Seeder/DmsCreditsSeeder.php
@@ -22,11 +22,6 @@ class DmsCreditsSeeder extends Seeder
         ]);
 
         Bouncer::ability()->create([
-            'name' => 'role_credits',
-            'title' => 'Roles with this ability can have credits.'
-        ]);
-
-        Bouncer::ability()->create([
             'name' => 'view_code_statistics',
             'title' => 'See statistics about created and used codes.'
         ]);
